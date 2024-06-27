440f64be7c3f3f3629325177612cb666025c637b
Soumya14041987 <soumya14041987@gmail.com>
2024-06-27 07:57:33 +0530

    create tables & columns

---
+++ migrations/V1.1.3_create_tables_columns.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE FDB_MNG_TRANSFORM_LYR.SCH_MNG_TL.Customers (
+  customer_id INT PRIMARY KEY,
+  first_name STRING,
+  last_name STRING,
+  email STRING,
+  phone_number STRING
+);
