6af242a5cab1159d6e3d3d1fa6374040866a2cf6
Soumya14041987 <soumya14041987@gmail.com>
2024-06-27 08:08:47 +0530

    added new sql script

---
+++ migrations/V1.1.4_create_tables_columns_add_column.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TABLE FDB_MNG_TRANSFORM_LYR.SCH_MNG_TL.Customers (
+  customer_id INT PRIMARY KEY,
+  first_name STRING,
+  last_name STRING,
+  email STRING,
+  phone_number STRING
+  pin_code STRING
+);
